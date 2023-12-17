7460365b72c67d93f61e48d5a4baff2e2d31e6be
RahulKathayat <rkathayat1103@gmail.com>
2023-12-17 23:34:54 +0530

    login and sign up

--- GlacierGuru/src/navigation/index.js
+++ GlacierGuru/src/navigation/index.js
@@ -4,13 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Welcome'>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Login'>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
