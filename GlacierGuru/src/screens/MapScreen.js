e89593b4eede14c2cb13733e63a7860d550d7800
RahulKathayat <rkathayat1103@gmail.com>
2023-12-19 17:02:39 +0530

    geolocation added

--- GlacierGuru/src/screens/MapScreen.js
+++ GlacierGuru/src/screens/MapScreen.js
@@ -1,26 +1,41 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React ,{useEffect,useState}from 'react'
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function MapScreen() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  useEffect(()=>{
+      Geolocation.getCurrentPosition((info) => {
+        setLatitude( info.coords.latitude);
+        setLongitude(info.coords.longitude);
+        console.log("lat: ",info.coords.latitude , "lon: ",info.coords.longitude);
+    }  
+);
+},[]);
+
   return (
     <View>
-      
+      {
+        latitude ?(
         <MapView
             style={{width:'100%',height:'100%'}}
             initialRegion={{
-                latitude: 30.6848588,
-                longitude: 76.6654077,
+                latitude: latitude,
+                longitude: longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
         >
             <Marker
-            coordinate={{latitude:30.6848588,longitude:76.6654077}}
+            coordinate={{latitude:latitude,longitude:longitude}}
             />
         </MapView>
-            
+        ):(<Text>no maps available</Text>)
+      } 
+
         
     </View>
   )
